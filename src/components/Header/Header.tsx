3fa1751ea88158ea76356539c4e4b2bbe02bd453
Kolya Movchan <kmovchan261@gmail.com>
2024-11-30 13:54:04 +0200

    fix: upgrade navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,11 +4,19 @@ const Header = () => {
   const { isAuthenticated } = useUser();
 
   return (
-    <header>
-      <div className="flex justify-between px-4">
-        <div>Logo</div>
+    <header className="bg-white w-full">
+      <div className="flex justify-between items-center py-4 px-6">
+        <div className="text-2xl font-bold text-blue-600">JobBoard</div>
         <div>
-          {isAuthenticated ? <button>Logout</button> : <button>Login</button>}
+          {isAuthenticated ? (
+            <button className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 transition duration-200">
+              Logout
+            </button>
+          ) : (
+            <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 transition duration-200">
+              Login
+            </button>
+          )}
         </div>
       </div>
     </header>
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -2,7 +2,7 @@ import { JobCard } from "@/components/JobCard";
 import { mockJobVacancies } from "@/mop/jobVacancy";
 const HomePage = () => {
   return (
-    <div>
+    <div className="container mx-auto flex flex-col gap-4 p-4">
       HomePage
       {mockJobVacancies.map((job) => (
         <JobCard {...job} />
