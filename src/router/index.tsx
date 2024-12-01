d9f4194cf602d429637982c5d40e5870365ca329
VoloshynDmytro95 <voloshincode@gmail.com>
2024-12-01 12:30:48 +0200

    update steps

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,11 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import LoginPage from "@/pages/Auth/LoginPage";
 import RegisterPage from "@/pages/Auth/RegisterPage";
-import HomePage from "@/pages/Home/HomePage";
 import AboutPage from "@/pages/About/AboutPage";
 import Manual from "@/pages/Auth/Manual/Manual";
 
-import RegisterForm from "@/pages/Auth/Form/RegisterForm";
 
 import VacancyPage from "@/pages/Vacancy/VacancyPage";
 import RegisterCredentialsPage from "@/pages/Auth/RegisterCredentialsPage";
@@ -21,7 +19,6 @@ export const AppRoutes = () => {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register-employee" element={<RegisterCredentialsPage />} />
       <Route path="/register/manual" element={<Manual />} />
-      <Route path="/register-form" element={<RegisterForm />} />
       <Route path="/login-employer" element={<EmployeLogin />} />
       <Route path="/contact-details" element={<ContactDetails />} />
       <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
