e7d1420393fa31459c54e7864a91527a42cbe4d7
Ivan Kucher <dev.kucher@gmail.com>
2024-12-01 21:15:12 +0200

    fix(api): host

--- src/api/api.ts
+++ src/api/api.ts
@@ -10,7 +10,7 @@ import {
 
 // Base Axios instance
 const api = axios.create({
-  baseURL: `http://localhost:3000/api/v1`,
+  baseURL: `https://hackaton-api.apps.devkucher.com/api/v1`,
   headers: {
     "Content-Type": "application/json",
   },
